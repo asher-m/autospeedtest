bdb7fa00efbd6c352416c1329fb7e53b82036240
Asher Merrill <asher.s.merrill@gmail.com>
2020-12-10 20:05:38 -0400

    ingest: add directory option for easier ingest

--- speedtest_ingestjson.py
+++ speedtest_ingestjson.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+import argparse
 import datetime
 import glob
 import json
+import os
 
 import speedtest_auto
 
 
-def main():
-    for f in glob.glob('speedtest_auto_tests/*.json'):
+def main(searchdir=None):
+    globhere = 'speedtest_auto_tests/*.json'
+    if searchdir and os.path.isdir(searchdir):
+        globhere = os.path.join(searchdir, '*.json')
+        print(f'Looking at the following directory for speedtest-cli json files:\n{globhere}')
+    for f in glob.glob(globhere):
         with open(f, 'r') as fp:
             line = fp.readline()
             d = json.loads(line)
@@ -26,4 +32,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('searchdir', action='store', nargs='?', default=None,
+                        help='directory to search for speedtest-cli json files')
+    kwargs = vars(parser.parse_args())
+    main(**kwargs)
